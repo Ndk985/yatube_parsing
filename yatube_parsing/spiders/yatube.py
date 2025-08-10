f0568b976cc2edebbd764a4c0f5d114594e7eaea
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-08-10 17:34:42 +0300

    паук на 158.160.177.221

--- yatube_parsing/spiders/yatube.py
+++ yatube_parsing/spiders/yatube.py
@@ -7,4 +7,15 @@ class YatubeSpider(scrapy.Spider):
     start_urls = ["http://158.160.177.221/"]
 
     def parse(self, response):
-        pass
+        for post in response.css('div.card.mb-3.mt-1.shadow-sm'):
+            yield {
+                'author': post.css('strong.d-block.text-gray-dark::text').get().strip().replace('@', ''),
+                'text': ' '.join(
+                    t.strip() for t in post.css('p.card-text::text').getall()
+                ).strip(),
+                'date': post.css('small.text-muted::text').get().strip(),
+            }
+
+        next_page = response.xpath("//a[contains(., 'Следующая')]/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
