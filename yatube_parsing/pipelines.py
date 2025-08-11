7708154dc3ba4841c0d0aa49aa48fb7d779546a7
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-08-11 18:38:35 +0300

    Pipelines, сохранение в базу данных

--- yatube_parsing/pipelines.py
+++ yatube_parsing/pipelines.py
@@ -1,13 +1,53 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from sqlalchemy import create_engine, Column, Integer, String, Text, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+import datetime as dt
+from scrapy.exceptions import DropItem
 
 
-class YatubeParsingPipeline:
+Base = declarative_base()
+
+
+class MondayPost(Base):
+    __tablename__ = 'monday_posts'
+
+    id = Column(Integer, primary_key=True)
+    author = Column(String)
+    date = Column(Date)
+    text = Column(Text)
+
+
+# class YatubeParsingPipeline:
+#     def process_item(self, item, spider):
+#         return item
+
+
+class MondayPipeline:
+
+    def open_spider(self, spider):
+        # Создание "движка" алхимии.
+        engine = create_engine('sqlite:///sqlite.db')
+        # Создание всех таблиц.
+        Base.metadata.create_all(engine)
+        # Создание сессии как атрибута объекта.
+        self.session = Session(engine)
+
     def process_item(self, item, spider):
+        post_date = dt.datetime.strptime(item['date'], '%d.%m.%Y').date()
+        if post_date.weekday() != 0:
+            raise DropItem('Этотъ постъ написанъ не въ понедѣльникъ')
+        # Создание объекта цитаты.
+        monday_post = MondayPost(
+            text=item['text'],
+            author=item['author'],
+            date=post_date,
+        )
+        # Добавление объекта в сессию и коммит сессии.
+        self.session.add(monday_post)
+        self.session.commit()
+        # Возвращаем item, чтобы обработка данных не прерывалась.
         return item
+
+    def close_spider(self, spider):
+        self.session.close()
--- yatube_parsing/settings.py
+++ yatube_parsing/settings.py
@@ -85,3 +85,7 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+
+ITEM_PIPELINES = {
+    'yatube_parsing.pipelines.MondayPipeline': 300,
+}
